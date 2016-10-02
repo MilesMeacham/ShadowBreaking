4f7e8194b08c6d8b9151ef4c680487e87d1d5066
Unknown <jubleshum@gmail.com>
2016-10-02 17:46:10 -0600

    Fixed PlayerMovement movement amoutn
    Scaled down how far the player moves

--- ShadowBreaking/Assets/Scripts/PlayerMovement.cs
+++ ShadowBreaking/Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,12 @@ public class PlayerMovement : MonoBehaviour {
 	Animator anim;
 
 
-	// Use this for initialization
 	void Start () {
 		rbody = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		Vector2 movement_vector = new Vector2 (Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
 
@@ -26,6 +25,6 @@ public class PlayerMovement : MonoBehaviour {
 			anim.SetBool ("IsWalking", false);
 		}
 
-		rbody.MovePosition (rbody.position + (movement_vector * 100) * Time.deltaTime);
+		rbody.MovePosition (rbody.position + (movement_vector) * Time.deltaTime);
 	}
 }
