00feec7d7cadc027ae926f10a40c4d52998956ff
Unknown <jubleshum@gmail.com>
2016-10-02 20:56:36 -0600

    Pulled changes from Develop into jshums

---
+++ ShadowBreaking/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,17 @@
+﻿// Author: Miles
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+
+	// Loads scene 1
+    // We will want to change this to load the next scene or something
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+}
