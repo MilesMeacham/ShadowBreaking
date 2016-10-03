052d9300929e1140501a1b0a10a97e03005724e7
Miles Meacham <littlemeacham@gmail.com>
2016-10-02 21:43:15 -0600

    Got the UI Hearts Working
    The heart system is working. I will need to talked with whoever is going to be scripting the health system to make these two work together

---
+++ ShadowBreaking/Assets/Scripts/UIHearts.cs
@@ -0,0 +1,45 @@
+﻿// Author: Miles
+// Purpose: Attach this script to the individual heart game objects. The HeartManager will call these functions.
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIHearts : MonoBehaviour {
+
+    public Sprite heartFull, heartHalf, heartEmpty;
+
+    private Image sprite;
+
+    void Start()
+    {
+        sprite = GetComponent<Image>();
+    }
+
+    public void DisplayFullHeart()
+    {
+        sprite.sprite = heartFull;
+    }
+
+    public void DisplayHalfHeart()
+    {
+        sprite.sprite = heartHalf;
+    }
+
+    public void DisplayEmptyHeart()
+    {
+        sprite.sprite = heartEmpty;
+    }
+
+    // These will be used when the player gets a max health up (Gets an extra heart container)
+    public void TurnRendererOff()
+    {
+        sprite.enabled = false;
+    }
+
+    public void TurnRendererOn()
+    {
+        sprite.enabled = true;
+    }
+
+}
