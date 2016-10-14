26311ce0f500f08916ca27ed29d4a52e2921693c
Brady Ericksen <bradyericksen@gmail.com>
2016-10-13 18:45:42 -0600

    Quick fix to EnemyManager

--- ShadowBreaking/Assets/Scripts/EnemyManager.cs
+++ ShadowBreaking/Assets/Scripts/EnemyManager.cs
@@ -8,7 +8,7 @@ public class EnemyManager : MonoBehaviour
     public float spawnTime = 5f;            // How long between each spawn.
     public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
 	public int maxSpawn = 7;
-	public int remainingSpawn = maxSpawn;
+	public int remainingSpawn;
 	public int deaths = 0;
 	public Text victory;
 
@@ -16,6 +16,7 @@ public class EnemyManager : MonoBehaviour
     void Start ()
     {
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+		remainingSpawn = maxSpawn;
         InvokeRepeating ("Spawn", spawnTime, spawnTime);
     }
 
