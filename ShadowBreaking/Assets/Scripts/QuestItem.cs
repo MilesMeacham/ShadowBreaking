6e46e1cccec2e30660b1f4a2ceaa73ad5c57c622
Unknown <nanobot18@gmail.com>
2016-10-23 13:20:53 -0600

    quest system
    Added simple quest system, NPC dialogue window, and associated assets.

---
+++ ShadowBreaking/Assets/Scripts/QuestItem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestItem : MonoBehaviour {
+    public GameObject QuestNPC_collider;
+    QuestDialogue questDialogue;
+
+    public AudioClip collectSFX;
+    AudioSource sfx;
+
+
+    // START:
+    void Start() {
+        sfx = GetComponent<AudioSource>();
+        sfx.clip = collectSFX;
+        questDialogue = QuestNPC_collider.GetComponent<QuestDialogue>();
+    }
+
+
+    // UPDATE:
+    void Update() { }
+
+
+    /// ON TRIGGER ENTER (2D):
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            Debug.Log("Player Collided!");
+            sfx.Play();
+            questDialogue.questComplete = true;
+            this.GetComponent<Collider2D>().enabled = false;
+            this.GetComponent<SpriteRenderer>().enabled = false;
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
