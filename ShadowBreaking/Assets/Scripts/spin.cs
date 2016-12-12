7e9bd070fa7655def1f700c8cbe91813d6b0ffe3
Unknown <nanobot18@gmail.com>
2016-12-12 00:32:59 -0700

    Added new quest text and a new scene called "Abyss".

---
+++ ShadowBreaking/Assets/Scripts/spin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spin : MonoBehaviour {
+    public float speed = 15.0f;
+    public bool counterclockwise = false;
+
+	// START:
+	void Start () {
+	
+	}
+	
+	// UPDATE:
+	void Update () {
+        if (counterclockwise) this.transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+        else this.transform.Rotate(Vector3.back * Time.deltaTime * speed);
+    }
+}
